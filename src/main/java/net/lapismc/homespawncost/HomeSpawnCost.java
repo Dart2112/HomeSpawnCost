15c03657a9af31a13417d9365e52b59d9da87569
notfoundname <48317169+notfoundname@users.noreply.github.com>
2020-01-18 14:03:05 +0800

    added bullshit code

--- src/main/java/net/lapismc/homespawncost/HomeSpawnCost.java
+++ src/main/java/net/lapismc/homespawncost/HomeSpawnCost.java
@@ -12,134 +12,194 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.io.File;
 import java.util.logging.Logger;
 
-public final class HomeSpawnCost extends JavaPlugin implements Listener {
+public final class HomeSpawnCost extends JavaPlugin implements Listener
+{
 
     private static Economy econ;
     private String currencySymbol;
     private Logger logger = getLogger();
 
     @Override
-    public void onEnable() {
+    public void onEnable()
+    {
         configs();
-        if (!setupEconomy()) {
-            logger.severe("Disabled due to no Vault dependency found!");
+        if (!setupEconomy())
+        {
+            logger.severe("We can't find Vault. Install it. Disabling!");
             getServer().getPluginManager().disablePlugin(this);
             return;
         }
-        currencySymbol = getConfig().getString("Messages.CurrencySymbol");
+        currencySymbol = getConfig().getString("messages.currency_symbol");
         Bukkit.getPluginManager().registerEvents(this, this);
     }
 
-    private boolean setupEconomy() {
-        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+    private boolean setupEconomy()
+    {
+        if (getServer().getPluginManager().getPlugin("Vault") == null)
+        {
             return false;
         }
         RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
-        if (rsp == null) {
+        
+        if (rsp == null)
+        {
             return false;
         }
         econ = rsp.getProvider();
         return econ != null;
     }
 
-    private void configs() {
+    private void configs() 
+    {
         File config = new File(getDataFolder(), "config.yml");
-        if (config.exists()) {
-            if (getConfig().getInt("ConfigVersion") != 1) {
+        
+        if (config.exists())
+        {
+            if (getConfig().getInt("config_version") != 2)
+            {
                 File configBackup = new File(getDataFolder(), "config_backup.yml");
                 config.renameTo(configBackup);
-                logger.info("Config has been updated, Please transfer values");
+                logger.info("Config has been updated, please transfer values.");
                 saveDefaultConfig();
             }
-        } else {
+        }
+        else
+        {
             saveDefaultConfig();
         }
+        logger.severe("Plugin has been enabled.");
+        logger.severe("Fork by: notfoundname11");
     }
 
-    private String getMessage(String key) {
+    private String getMessage(String key) 
+    {
         return ChatColor.translateAlternateColorCodes('&', getConfig().getString(key));
     }
 
     @EventHandler
-    public void onHomeMove(HomeMoveEvent e) {
-        if (getConfig().getBoolean("Charging.Moving")) {
+    public void onHomeMove(HomeMoveEvent e) 
+    {
+        if (getConfig().getBoolean("use.moving_home")) 
+        {
             Double balance = econ.getBalance(Bukkit.getOfflinePlayer(e.getPlayer().getUniqueId()));
-            Integer cost = e.getName().equals("Home") ? getConfig().getInt("Costs.MoveMainHome") : getConfig().getInt("Costs.MoveCustomHome");
-            if (balance < cost) {
-                e.setCancelled(true, getMessage("Messages.NotEnoughMoney").replace("%COST%", currencySymbol + cost).replace("%Balance%", currencySymbol + balance));
-            } else {
-                if (econ.withdrawPlayer(Bukkit.getOfflinePlayer(e.getPlayer().getUniqueId()), cost).transactionSuccess()) {
-                    e.getPlayer().sendMessage(getMessage("Messages.MoneySpent").replace("%COST%", currencySymbol + cost).replace("%ACTION%", "move your home"));
-                } else {
-                    e.setCancelled(true, getMessage("Messages.NotEnoughMoney").replace("%COST%", currencySymbol + cost).replace("%Balance%", currencySymbol + balance));
+            Integer cost = getConfig().getInt("cost.move_home");
+            
+            if (balance < cost) 
+            {
+                e.setCancelled(true, getMessage("messages.not_enough_money").replace("%cost%", currencySymbol + cost).replace("%balance%", currencySymbol + balance));
+            }
+            else
+            {
+                if (econ.withdrawPlayer(Bukkit.getOfflinePlayer(e.getPlayer().getUniqueId()), cost).transactionSuccess())
+                {
+                    e.getPlayer().sendMessage(getMessage("messages.move_home").replace("%cost%", currencySymbol + cost).replace("%name%", e.getName()));
+                }
+                else
+                {
+                    e.setCancelled(true, getMessage("messages.not_enough_money").replace("%cost%", currencySymbol + cost).replace("%balance%", currencySymbol + balance));
                 }
             }
         }
     }
 
     @EventHandler
-    public void onHomeSet(HomeSetEvent e) {
-        if (getConfig().getBoolean("Charging.Setting")) {
+    public void onHomeSet(HomeSetEvent e) 
+    {
+        if (getConfig().getBoolean("use.setting_home"))
+        {
             Double balance = econ.getBalance(Bukkit.getOfflinePlayer(e.getPlayer().getUniqueId()));
-            Integer cost = e.getHome().getName().equals("Home") ? getConfig().getInt("Costs.SetMainHome") : getConfig().getInt("Costs.SetCustomHome");
-            if (balance < cost) {
-                e.setCancelled(true, getMessage("Messages.NotEnoughMoney").replace("%COST%", currencySymbol + cost).replace("%Balance%", currencySymbol + balance));
-            } else {
-                if (econ.withdrawPlayer(Bukkit.getOfflinePlayer(e.getPlayer().getUniqueId()), cost).transactionSuccess()) {
-                    e.getPlayer().sendMessage(getMessage("Messages.MoneySpent").replace("%COST%", currencySymbol + cost).replace("%ACTION%", "set your home"));
-                } else {
-                    e.setCancelled(true, getMessage("Messages.NotEnoughMoney").replace("%COST%", currencySymbol + cost).replace("%Balance%", currencySymbol + balance));
+            Integer cost =getConfig().getInt("cost.set_home");
+            
+            if (balance < cost)
+            {
+                e.setCancelled(true, getMessage("messages.not_enough_money").replace("%cost%", currencySymbol + cost).replace("%balance%", currencySymbol + balance));
+            }
+            else
+            {
+                if (econ.withdrawPlayer(Bukkit.getOfflinePlayer(e.getPlayer().getUniqueId()), cost).transactionSuccess())
+                {
+                    e.getPlayer().sendMessage(getMessage("messages.set_home").replace("%cost%", currencySymbol + cost).replace("%home%", e.getHome().getName()));
+                }
+                else
+                {
+                    e.setCancelled(true, getMessage("messages.not_enough_money").replace("%cost%", currencySymbol + cost).replace("%balance%", currencySymbol + balance));
                 }
             }
         }
     }
 
     @EventHandler
-    public void onHomeDelete(HomeDeleteEvent e) {
-        if (getConfig().getBoolean("Charging.Deleting")) {
+    public void onHomeDelete(HomeDeleteEvent e)
+    {
+        if (getConfig().getBoolean("use.deleting_home"))
+        {
             Double balance = econ.getBalance(Bukkit.getOfflinePlayer(e.getPlayer().getUniqueId()));
-            Integer cost = e.getHome().getName().equals("Home") ? getConfig().getInt("Costs.DeleteMainHome") : getConfig().getInt("Costs.DeleteCustomHome");
-            if (balance < cost) {
-                e.setCancelled(true, getMessage("Messages.NotEnoughMoney").replace("%COST%", currencySymbol + cost).replace("%Balance%", currencySymbol + balance));
-            } else {
-                if (econ.withdrawPlayer(Bukkit.getOfflinePlayer(e.getPlayer().getUniqueId()), cost).transactionSuccess()) {
-                    e.getPlayer().sendMessage(getMessage("Messages.MoneySpent").replace("%COST%", currencySymbol + cost).replace("%ACTION%", "delete your home"));
-                } else {
-                    e.setCancelled(true, getMessage("Messages.NotEnoughMoney").replace("%COST%", currencySymbol + cost).replace("%Balance%", currencySymbol + balance));
+            Integer cost = getConfig().getInt("cost.deleting_home");
+            
+            if (balance < cost)
+            {
+                e.setCancelled(true, getMessage("messages.not_enough_money").replace("%cost%", currencySymbol + cost).replace("%balance%", currencySymbol + balance));
+            }
+            else
+            {
+                if (econ.withdrawPlayer(Bukkit.getOfflinePlayer(e.getPlayer().getUniqueId()), cost).transactionSuccess())
+                {
+                    e.getPlayer().sendMessage(getMessage("messages.delete_home").replace("%cost%", currencySymbol + cost).replace("%home%", e.getHome().getName()));
+                }
+                else
+                {
+                    e.setCancelled(true, getMessage("messages.not_enough_money").replace("%cost%", currencySymbol + cost).replace("%balance%", currencySymbol + balance));
                 }
             }
         }
     }
 
     @EventHandler
-    public void onHomeRename(HomeRenameEvent e) {
-        if (getConfig().getBoolean("Charging.Deleting")) {
+    public void onHomeRename(HomeRenameEvent e) 
+    {
+        if (getConfig().getBoolean("use.renaming_home")) 
+        {
             Double balance = econ.getBalance(Bukkit.getOfflinePlayer(e.getPlayer().getUniqueId()));
-            Integer cost = getConfig().getInt("Costs.RenameHome");
-            if (balance < cost) {
-                e.setCancelled(true, getMessage("Messages.NotEnoughMoney").replace("%COST%", currencySymbol + cost).replace("%Balance%", currencySymbol + balance));
-            } else {
-                if (econ.withdrawPlayer(Bukkit.getOfflinePlayer(e.getPlayer().getUniqueId()), cost).transactionSuccess()) {
-                    e.getPlayer().sendMessage(getMessage("Messages.MoneySpent").replace("%COST%", currencySymbol + cost).replace("%ACTION%", "rename your home"));
-                } else {
-                    e.setCancelled(true, getMessage("Messages.NotEnoughMoney").replace("%COST%", currencySymbol + cost).replace("%Balance%", currencySymbol + balance));
+            Integer cost = getConfig().getInt("cost.rename_home");
+            
+            if (balance < cost) 
+            {
+                e.setCancelled(true, getMessage("messages.not_enough_money").replace("%cost%", currencySymbol + cost).replace("%balance%", currencySymbol + balance));
+            }
+            else
+            {
+                if (econ.withdrawPlayer(Bukkit.getOfflinePlayer(e.getPlayer().getUniqueId()), cost).transactionSuccess())
+                {
+                    e.getPlayer().sendMessage(getMessage("messages.rename_home").replace("%cost%", currencySymbol + cost).replace("%old_name%", e.getOldHome()).replace("%new_name%", e.getNewHome()));
+                }
+                else
+                {
+                    e.setCancelled(true, getMessage("messages.not_enough_money").replace("%cost%", currencySymbol + cost).replace("%balance%", currencySymbol + balance));
                 }
             }
         }
     }
 
     @EventHandler
-    public void onHomeTeleport(HomeTeleportEvent e) {
-        if (getConfig().getBoolean("Charging.Teleporting")) {
+    public void onHomeTeleport(HomeTeleportEvent e)
+    {
+        if (getConfig().getBoolean("use.teleport_home"))
+        {
             Double balance = econ.getBalance(Bukkit.getOfflinePlayer(e.getPlayer().getUniqueId()));
-            Integer cost = e.getHome().getName().equals("Home") ? getConfig().getInt("Costs.TeleportMainHome") : getConfig().getInt("Costs.TeleportCustomHome");
-            if (balance < cost) {
-                e.setCancelled(true, getMessage("Messages.NotEnoughMoney").replace("%COST%", currencySymbol + cost).replace("%Balance%", currencySymbol + balance));
-            } else {
-                if (econ.withdrawPlayer(Bukkit.getOfflinePlayer(e.getPlayer().getUniqueId()), cost).transactionSuccess()) {
-                    e.getPlayer().sendMessage(getMessage("Messages.MoneySpent").replace("%COST%", currencySymbol + cost).replace("%ACTION%", "teleport to your home"));
-                } else {
-                    e.setCancelled(true, getMessage("Messages.NotEnoughMoney").replace("%COST%", currencySymbol + cost).replace("%Balance%", currencySymbol + balance));
+            Integer cost = getConfig().getInt("cost.teleport_home");
+            if (balance < cost)
+            {
+                e.setCancelled(true, getMessage("messages.no_enough_money").replace("%cost%", currencySymbol + cost).replace("%balance%", currencySymbol + balance));
+            }
+            else
+            {
+                if (econ.withdrawPlayer(Bukkit.getOfflinePlayer(e.getPlayer().getUniqueId()), cost).transactionSuccess())
+                {
+                    e.getPlayer().sendMessage(getMessage("messages.teleport_home").replace("%cost%", currencySymbol + cost).replace("%name%", e.getHome().getName()));
+                }
+                else
+                {
+                    e.setCancelled(true, getMessage("messages.not_enough_money").replace("%cost%", currencySymbol + cost).replace("%balance%", currencySymbol + balance));
                 }
             }
         }
